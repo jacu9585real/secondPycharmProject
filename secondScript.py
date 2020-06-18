7872fc897962666a57aea5b4351adfed8304021c
jacu9585real <60018531+jacu9585real@users.noreply.github.com>
2020-06-18 12:34:28 -0600

    Added new classes to control all energy drinks and their actions you can perform with them

--- secondScript.py
+++ secondScript.py
@@ -7,3 +7,34 @@ for x in range(10):
     print(myVar)
 
 
+class energyDrink:
+    def __init__(self, shape, color, ounces="12"):
+        self.ounces = ounces
+        self.shape = shape
+        self.color = color
+        print("NEW CAN HAS BEEN CREATED :)")
+    def recycle(self):
+        print("clang clang clang!")
+        self.shape = "flat"
+        self.ounces = 0
+
+class monster(energyDrink):
+    def drink(self):
+        print("SSSUUUUUUPER ENERGY BOD")
+
+class reign(energyDrink):
+    def drink(self):
+        print("You have choosen to ingest reign.. do not pass go do not collect $200 and don't sleep ever again")
+        self.ounces = 0
+    def recycle(self):
+        print("reign cannot be recycled it is technically hazardous waste...")
+
+myDrink = reign("orange", "cylinder")
+print(myDrink.ounces)
+myDrink.recycle()
+print(myDrink.ounces)
+
+
+
+
+
