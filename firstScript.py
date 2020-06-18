3fc458e6a846751705a32dc046c0485fabde2e95
jacu9585real <60018531+jacu9585real@users.noreply.github.com>
2020-06-18 11:32:53 -0600

    Initial commit

---
+++ firstScript.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+myNump = [2,3,4,5]
+print(myNump)
+
+
+class mySolarSystem:
+    def __init__(self, numPlanets, age, size):
+        self.numPlanets = numPlanets
+        self.age = age
+        self.size = size
+        self.defaultColors = ["blue", "red", "green"]
+
+    def blowup(self):
+        self.numPlanets = 0
+        self.size = self.size+100
+        print("ALL PLANETS HAVE BEEN DESTROYED :(")
+
+
+wanker = mySolarSystem(10, 300, 5000)
+print(wanker.numPlanets)
+wanker.blowup()
+print(wanker.numPlanets)
+print(wanker.defaultColors)
+
+
+
